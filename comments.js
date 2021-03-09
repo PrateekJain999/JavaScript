09e4dcf389b3a2f58bbfe9eb57b7df352b796300
Prateek Jain <jain22719@gmail.com>
2021-03-09 21:43:34 +0530

    js

---
+++ comments.js
@@ -0,0 +1,15 @@
+// This comment occupies a line of its own
+alert('Hello');
+
+alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+alert('Hello');
+alert('World');
+
+/* Commenting out the code
+alert('Hello');
+*/
+alert('World');
