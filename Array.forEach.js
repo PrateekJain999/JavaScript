bd17e293622f62a451e98e3b499deb7173413060
Prateek Jain <jain22719@gmail.com>
2021-03-10 12:30:46 +0530

    js

---
+++ Array.forEach.js
@@ -0,0 +1,5 @@
+const ar=[1,2,3,4]
+
+a=ar.forEach((ele)=>{
+  console.log(ele)
+})
---
+++ Array.includes.js
@@ -0,0 +1,13 @@
+const ar=[1,2,3,4]
+
+a=ar.includes(3,0)
+console.log(a)
+
+a=ar.includes(5,0)
+console.log(a)
+
+a=ar.includes(3)
+console.log(a)
+
+a=ar.includes(5,5)
+console.log(a)
