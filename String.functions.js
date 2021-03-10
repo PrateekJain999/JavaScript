168289450e22ada2a7f8d0e6b59267a7b1d6ee44
Prateek Jain <jain22719@gmail.com>
2021-03-10 14:59:24 +0530

    js

---
+++ String.functions.js
@@ -0,0 +1,30 @@
+const str="Prateek Jain"
+
+console.log(str.charAt(4))
+console.log(str.charCodeAt(4))
+console.log(str.concat(" is ","a"," Good ","Boy"))
+console.log(str)
+console.log(str.indexOf('e'))
+console.log(str.lastIndexOf('e'))
+console.log(str.search('t'))
+console.log(str.match(/a/g))
+console.log(str.match(/a/i))
+console.log(str.match(/a/m))
+console.log(str.match(/[a-z]/g))
+console.log(str.replace('e','a'))
+console.log(str)
+console.log(str.substr(1,4))//start->length
+console.log(str.substring(1,4))//start->end
+console.log(str.slice(2,4))
+console.log(str)
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
+console.log(str.toLocaleLowerCase())
+console.log(str.toLocaleUpperCase())
+console.log(str)
+console.log(str.toString())
+console.log(str.valueOf())//find out the primitive value of String object
+console.log(str.split(" "))
+console.log(str)
+console.log(str.trim())// trim left and right side space not middle one
+console.log(str)
