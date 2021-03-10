32cf356842266b2b5ad8f246be1de108206a58ba
Prateek Jain <jain22719@gmail.com>
2021-03-10 15:42:23 +0530

    js

---
+++ NumberObject.js
@@ -0,0 +1,15 @@
+console.log(a.isFinite(67))
+console.log(Number.isInteger(67))
+console.log(Number.parseFloat("67"))
+console.log(Number.parseInt("78.000",2))
+a=87585
+console.log(a.toExponential(3))
+
+a=87585
+console.log(a.toExponential(3))
+b=28484.383
+console.log(b.toFixed(1))
+c=1933.11
+console.log(c.toPrecision(1))
+d=23.5
+console.log(d.toString(2))
