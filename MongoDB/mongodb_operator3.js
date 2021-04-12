ad9aaf6e146b2378b056726d4ab9cdfa46db3f71
Prateek Jain <jain22719@gmail.com>
2021-04-12 14:15:04 +0530

    mongo

--- MongoDB/mongodb_operator3.js
+++ MongoDB/mongodb_operator3.js
@@ -389,6 +389,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
         //         console.log(data)
         //     })
 
+        //================================= set experssion ===================================
+        // col.aggregate(
+        //     [
+        //         { $match: { 'firstName': 'Prashuk' } },
+        //         {
+        //             $project: {
+        //                 'allElementsTrue1': { $allElementsTrue: ['$array'] },
+        //                 'allElementsTrue2': { $allElementsTrue: [[]] },
+        //                 'allElementsTrue3': { $allElementsTrue: [[false, 0]] },
+        //                 'anyElementTrue1': { $anyElementTrue: ['$array'] },
+        //                 'anyElementTrue2': { $anyElementTrue: [[true, false]] },
+        //                 'anyElementTrue3': { $anyElementTrue: [[false, false]] },
+        //                 'setDifference': { $setDifference: [[1, 2, 3, 4], [2, 3, 7, 8]] },
+        //                 'setEquals1': { $setEquals: [[1, 2, 3, 4], [2, 3, 7, 8]] },
+        //                 'setIntersection': { $setIntersection: [[1, 2, 3, 4], [2, 3, 7, 8]] },
+        //                 'setIsSubset': { $setIsSubset: [[1, 2, 3, 4], [1, 2]] },
+        //                 'setUnion': { $setUnion: [[1, 2, 3, 4], [2, 3, 7, 8]] },
+        //             }
+        //         }
+        //     ])
+
+        
 
     })
 })
